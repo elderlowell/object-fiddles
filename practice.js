262b68d70c498f8d5a82564bb4665a739c52f929
Jonathan Lowell <elderlowell@gmail.com>
2017-05-09 19:47:30 -0600

    Finished problems 2-3

--- practice.js
+++ practice.js
@@ -22,16 +22,28 @@
 //Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
 
   //Code here
+  var favoriteThings = {
+    band: 'Ben Folds',
+    food: 'Pabellón criollo',
+    person: 'Jennifer',
+    book: 'The Princess Bride',
+    movie: 'The Bourne Identity',
+    holiday: 'Saint Patrick\'s Day'
+  };
 
 
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
   //Code here
+  favoriteThings.car = 'Tesla Model X';
+  favoriteThings.brand = 'Apple';
 
 
 //Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 
   //Code here
+  favoriteThings.food = 'Chicken Nuggets';
+  favoriteThings.book = 'Harry Potter';
 
 
 
@@ -49,16 +61,22 @@ Using dot notation, add another key (or property) to your backPack object
 that is named color, with the value being the color of your backpack. */
 
   //Code here
+  var backPack = {};
+  var item = 'firstPocket';
+  backPack[item] = 'chapstick';
+  backPack.color = 'gray';
 
 //After you do the above, alert your entire backPack object.
 
   //Code here
+  alert(backPack);
 
 /*You probably noticed that it just alerted [object Object].
 Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. */
 
   //Code here
+  console.log(backPack);
 
 
 
